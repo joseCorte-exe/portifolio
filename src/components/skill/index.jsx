84099a68530a286a9d9248da0bf6054848a4a412
joseCorte-exe <josecorte-corte@hotmail.com>
2021-12-21 12:00:11 -0300

    :sparkles: skillPage Responsive

--- src/components/skill/index.jsx
+++ src/components/skill/index.jsx
@@ -1,42 +1,30 @@
 import React from "react";
 
-import Line from "../line";
-
 import jsSVG from "../../imgs/javascript.svg";
 import nodeSVG from "../../imgs/nodejs.svg";
 import reactSVG from "../../imgs/react.svg";
 import figmaSVG from "../../imgs/figma.svg";
 import htmlSVG from "../../imgs/html.svg";
 import cssSVG from "../../imgs/css.svg";
-
-import mediapipePNG from "../../imgs/mediapipe.png";
-import pythonSVG from "../../imgs/python.svg";
-import opencvSVG from "../../imgs/opencv.svg";
+import next from "../../imgs/next.png";
 
 import "./index.css";
 
 export default function Skills() {
     return (
-        <div className="skills-container">
-            <div className="skills-content">
-                <h1 className="white-f9 skills-topic">My skills</h1>
-                <div className="skills-subcontent">
-                    <div className="skills-web">  
-                        <img src={cssSVG}/>
-                        <img src={htmlSVG}/>
-                        <img src={jsSVG}/>
-                        <img src={nodeSVG}/>
-                        <img src={reactSVG}/>
-                        <img src={figmaSVG}/>
-                    </div>
-                    <Line />
-                    <div className="skills-dataScience">
-                        <img src={pythonSVG}/>
-                        <img src={opencvSVG}/>
-                        <img src={mediapipePNG}/>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <main className='skills-main' >
+            <section className='skills-section'>
+                <h1 className="primary topic skills">My skills</h1>
+                <article className="skills-article">
+                    <img src={cssSVG}/>
+                    <img src={htmlSVG}/>
+                    <img src={jsSVG}/>
+                    <img src={nodeSVG}/>
+                    <img src={reactSVG}/>
+                    <img src={figmaSVG}/>
+                    <img src={next}/>
+                </article>
+            </section>
+        </main>
     )
 }
