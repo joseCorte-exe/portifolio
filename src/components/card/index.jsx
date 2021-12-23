1a79567427de56403b87acd4e6629f686df45b93
joseCorte-exe <josecorte-corte@hotmail.com>
2021-12-22 21:42:34 -0300

    :art: changing classes

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -6,7 +6,7 @@ import ptPrw from "../../imgs/portifolio.png";
 export default function Card() {
     return (
         <main className="prj-main" >
-            <img src={ptPrw} alt="" width="200" />
+            <img src={ptPrw} alt="" width="200" className='card-img' />
             <section className="prj-section" >
                 <h1>Card Title</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
